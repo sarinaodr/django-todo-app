ae742da388e5fc1152c572d58c4fb88991b55f6d
sarina <sarinaomidvar99@gmail.com>
2023-08-28 17:53:16 +0330

    end of the project

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,10 +3,10 @@ from django import forms
 from .models import Task
 
 
-class TaskForm(forms.Form):
+class TaskForm(forms.ModelForm):
     title = forms.CharField(widget = forms.TextInput(attrs={'placeholder':'add new task'}))
-    priority = forms.ChoiceField()
+    priority = forms.Select()
     
     class Meta:
         model = Task
-        fields = 'title' , 'user' , 'priority'
+        fields = 'title' , 'user', 'priority'
